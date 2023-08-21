533a5a4c0a811cd6a51b7c1c46ca3358fd3fada6
Victor Santos Rohod <victorsantosrohod@gmail.com>
2023-08-21 07:40:09 -0300

    Progredindo EcmaScript 8 e 9
    Colocando mais um Progresso no curso

--- Javascript/Javascript - EcmaScript 8 e 9 novas formas de escrever em Js/src/js/array.js
+++ Javascript/Javascript - EcmaScript 8 e 9 novas formas de escrever em Js/src/js/array.js
@@ -3,15 +3,14 @@ const aprovados = ['Marcelo', 'Aline', 'Gustavo', 'Ricardo'];
 for (let nome of aprovados){
     if (nome === 'Gustavo'){
         console.log("Aprovado");
-        return true;
     }else{
-        console.log("Não encontrado")
+        console.log("Não encontrado");
     }
 }
 
 // metodo 2
 const nomeIndexOf = aprovados.indexOf("Gustavo");
-console.log(nomeIndexOf)
+console.log(nomeIndexOf);
 
 // adição do ecma
 
@@ -20,6 +19,6 @@ const nomeAprovado = aprovados.includes("Gustavo");
 if(nomeAprovado){
     console.log("Aprovado");
 }else{
-    console.log("Não aprovado")
+    console.log("Não aprovado");
 }
-console.log(nomeAprovado)
+console.log(nomeAprovado);
--- Javascript/Javascript - EcmaScript 8 e 9 novas formas de escrever em Js/src/js/object.js
+++ Javascript/Javascript - EcmaScript 8 e 9 novas formas de escrever em Js/src/js/object.js
@@ -1,11 +1,17 @@
 const cursos = {
-    node : 90,
-    angular : 90,
+    node : 50,
+    angular : 70,
     react : 90,
-    backbone : 90,
-}
+    backbone : 80,
+};
 
 const cursosChaves = Object.keys(cursos);
 document.getElementById("cursos").innerHTML = `<ul>${cursosChaves.map(curso => `<li>${curso}</li>`)}</ul>`;
 
-console.log(Object.entries(cursos))
+const cursosEntries = Object.entries(cursos);
+
+document.getElementById("cursosEntries").innerHTML = `<ul>${cursosEntries.map(curso => `<li>${curso[0]} - ${curso[1]} alunos</li>`)}</ul>`;
+
+const cursosMap = new Map(cursosEntries);
+console.log(cursosMap.get('node'));
+console.log(cursosMap.has('java'));
