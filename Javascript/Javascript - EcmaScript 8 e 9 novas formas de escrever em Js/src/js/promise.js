288c73e9d81a44f3b33db07b3348167a56f1a824
Victor Santos Rohod <victorsantosrohod@gmail.com>
2023-08-22 13:52:41 -0300

    Promise finally
    upando arquivos de estudos

---
+++ Javascript/Javascript - EcmaScript 8 e 9 novas formas de escrever em Js/src/js/promise.js
@@ -0,0 +1,4 @@
+(function listaLivrosJavascript(){
+    return fetch('https://www.googleapis.com/books/v1/volumes?q=javascript').then(data => data.json()).then(data => data.items.map(item => item)).then(data => data.map(item => item.volumeInfo)).then(data => data.map(item => item.title)).then(data => renderizarLista("promiseListaLivro", data)).catch(erro => console.error("Deu erro: "+erro)).finally(()=> console.log("Foi executado a função com sucesso!!!"));
+    // (); executa assim que compila
+})();
