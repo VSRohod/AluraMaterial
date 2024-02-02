2fe6ece590f0889ad7aea92ea4ba119315a19346
Victor Santos Rohod <89645335+VSRohod@users.noreply.github.com>
2024-02-02 11:21:26 -0300

    Progresso do curso Python: avance na Orientação a Objetos e consuma API
    Upando arquivos

--- PROGRAMAÇÃO/Aprenda a programar em Python com Orientação a Objetos/Python avance na Orientação a Objetos e consuma API/oo-sabor-express/app.py
+++ PROGRAMAÇÃO/Aprenda a programar em Python com Orientação a Objetos/Python avance na Orientação a Objetos e consuma API/oo-sabor-express/app.py
@@ -4,11 +4,14 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Praça','Gourmet')
 bebida_suco = Bebida('Suco de melancia', 5.0, 'grande')
-prato_paozinho = Bebida('Paozinho', 2.0, 'O melhor pao da cidade')
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pao da cidade')
+bebida_suco.aplicar_desconto()
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
